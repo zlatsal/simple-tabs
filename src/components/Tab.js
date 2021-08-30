05557de156161ea2efb02872cf72e09921b0768d
zlatsal <zlatans1987@gmail.com>
2021-08-30 14:51:16 +0200

    simple-tabs upload to github

---
+++ src/components/Tab.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+export default function Tab({ children }) {
+    const [highlightStyle,
+        setHighlightStyle] = useState({ left: 0,
+            opacity: 0});
+    
+    function moveHighlight(e) {
+      setHighlightStyle({
+        left: e.nativeEvent.layerX -150,
+      });
+    }
+    
+    function hideHighlight(e) {
+      setHighlightStyle({
+        opacity: 0,
+        left: e.nativeEvent.layerX -150,
+      });
+    }
+  
+    return (
+      <div className="tab" onMouseOut={hideHighlight} onMouseMove={moveHighlight}>
+        <div className="highlight" style={highlightStyle} />
+            {children}
+        </div> 
+    );
+  }
